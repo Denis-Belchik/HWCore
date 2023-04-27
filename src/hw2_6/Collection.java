dbee7123c2c98a906c2c11c03bfe3259dafba1cb
Setlocal1989 <denis1989g@mail.ru>
2023-04-27 15:56:40 +0400

    FIX collection

--- src/hw2_6/Collection.java
+++ src/hw2_6/Collection.java
@@ -43,8 +43,33 @@ public class Collection {
 
     private static void task4() {
         List<String> stringList = new ArrayList<>(List.of("qaz", "wsx", "qaz", "qaz", "edc", "wsx"));
-        Set<String> stringSet = new HashSet<>(stringList);
-        System.out.println(stringList.size() - stringSet.size());
+        Map<String, Integer> mapa = new HashMap<>();
+        for (String list : stringList) {
+            if (mapa.containsKey(list)) {
+                mapa.put(list, mapa.get(list) + 1);
+            } else {
+                mapa.put(list, 0);
+            }
+        }
+        System.out.println(mapa);
+
+//        Collections.sort(stringList);
+//        String temp = "";
+//        int count = 0;
+//        for (String val : stringList) {
+//            if (!temp.equals(val)) {
+//                //System.out.println(count);
+//                temp = val;
+//                count = 0;
+//
+//            } else {
+//                count++;
+//
+//            }
+//            System.out.println(count);
+//        }
+
+
     }
 
 
